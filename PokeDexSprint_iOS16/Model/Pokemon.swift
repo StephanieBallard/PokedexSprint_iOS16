2332fdba3524c86f888e466ea060989a29838049
Stephanie Ballard <user.name>
2020-04-10 09:00:00 -0700

    first commit, storyboard and files created

---
+++ PokeDexSprint_iOS16/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokeDexSprint_iOS16
+//
+//  Created by Stephanie Ballard on 4/10/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
---
+++ PokeDexSprint_iOS16/View/PokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTableViewCell.swift
+//  PokeDexSprint_iOS16
+//
+//  Created by Stephanie Ballard on 4/10/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
